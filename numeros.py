e4f3fb9c969e24b1a0a3dd22a2747abc6f21413b
giobarros <116304849+giobarros@users.noreply.github.com>
2022-10-20 17:07:57 -0300

    Add files via upload

---
+++ numeros.py
@@ -0,0 +1,13 @@
+numero_extenso = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'
+while True:
+    numero = int(input('Digite um número entre 0 e 10: '))
+    if not 0 <= numero <= 20:
+        print('Tente novamente...', end=' ')
+    else:
+        for pos, c in enumerate(numero_extenso):
+            if pos == numero:
+                print('Você digitou o número {}.'.format(c))
+    c = str(input('Quer continuar [S/N]? ')).strip().upper()[0]
+    if c == 'N':
+        break
+print('FIM!')
