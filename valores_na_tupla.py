48b46a751e57497a0a6094c60af2ecd5b472b9bb
Giovanna Barros <116304849+giobarros@users.noreply.github.com>
2022-10-20 19:10:53 -0300

    Delete valores_na_tupla.py

--- valores_na_tupla.py
+++
@@ -1,7 +0,0 @@
-tupla = (int(input('Digite um número: ')),
-        int(input('Digite outro número: ')),
-        int(input('Digite mais um número: ')),
-        int(input('Digite outro número: ')))
-print('Valores na tupla: {}'.format(tupla))
-print('O valor 9 aparece {} vez(es)'.format(tupla.count(9)))
-print('O primeiro número 3 aparece na posição {}'.format(tupla.index(3)))
